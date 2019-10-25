51d6619e65ec882e218808022f67902182526cef
Alexander Slesarenko <avslesarenko@gmail.com>
2019-10-25 23:48:48 +0300

    added configuration  reflect-config.json for PrepareBox (except Coll)

--- run-prove.sh
+++ run-prove.sh
@@ -2,15 +2,22 @@
 
 sbt assembly
 
+#native-image --no-server -cp target/scala-2.12/ergo-polyglot-3.0.0.jar \
+#  --report-unsupported-elements-at-runtime --no-fallback \
+#  -H:+TraceClassInitialization -H:+ReportExceptionStackTraces -H:+AddAllCharsets \
+#  -H:+AllowVMInspection -H:-RuntimeAssertions --enable-url-protocols=http,https \
+#  --allow-incomplete-classpath \
+#  --initialize-at-run-time=org.ergoplatform.polyglot.impl.SignedTransactionImpl \
+#  org.ergoplatform.polyglot.ni.Prove prove
+
 native-image --no-server -cp target/scala-2.12/ergo-polyglot-3.0.0.jar \
   --report-unsupported-elements-at-runtime --no-fallback \
   -H:+TraceClassInitialization -H:+ReportExceptionStackTraces -H:+AddAllCharsets \
   -H:+AllowVMInspection -H:-RuntimeAssertions --enable-url-protocols=http,https \
   --allow-incomplete-classpath \
-  --initialize-at-run-time=org.ergoplatform.polyglot.impl.SignedTransactionImpl \
-  org.ergoplatform.polyglot.ni.Prove prove
+  org.ergoplatform.polyglot.PrepareBox preparebox
 
 #  --initialize-at-build-time=scala.runtime.LambdaDeserialize,scala.runtime.LambdaDeserializer$ \
 #  --initialize-at-run-time=org.ergoplatform.polyglot.impl.SignedTransactionImpl \
 
-./prove -Djava.library.path=${GRAAL_HOME}/jre/lib
+./preparebox -Djava.library.path=${GRAAL_HOME}/jre/lib
