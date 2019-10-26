047b40ce465eebdfe8edb456683f4b463de9073a
Alexander Slesarenko <avslesarenko@gmail.com>
2019-10-26 14:32:23 +0300

    added missing reflection declarations

--- src/main/scala/org/ergoplatform/polyglot/PrepareBox.scala
+++ src/main/scala/org/ergoplatform/polyglot/PrepareBox.scala
@@ -1,18 +1,9 @@
 package org.ergoplatform.polyglot
 
-import org.ergoplatform.ErgoBox
-import org.ergoplatform.wallet.serialization.JsonCodecsWrapper
-import scalan.util.FileUtil.{file, read}
-import JsonCodecsWrapper._
-
 object PrepareBox  {
 
   def main(args: Array[String]) = {
     import MockData._
-    val boxTemplate = read(file(boxFile))
-    val boxJson = io.circe.parser.parse(boxTemplate).toOption.get
-    val box = boxJson.as[ErgoBox]
-      .getOrElse(sys.error(s"Cannot read box template from $boxFile"))
 
     val res = FileMockedRunner(infoFile, lastHeadersFile, boxFile).run { ctx =>
       val mockTxB = ctx.newTxBuilder()
