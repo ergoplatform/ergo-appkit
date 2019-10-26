c148a607b6dfa40c17931f1ee1f6aa58a83b5bfe
Alexander Slesarenko <avslesarenko@gmail.com>
2019-10-26 21:08:57 +0300

    implemented PrepareBox.js

--- examples/src/main/java/org/ergoplatform/example/PrepareBox.js
+++ examples/src/main/java/org/ergoplatform/example/PrepareBox.js
@@ -1,4 +1,24 @@
-
 const MockData = Java.type("org.ergoplatform.example.MockData")
+const FileMockedRunner = Java.type("org.ergoplatform.example.util.FileMockedRunner")
+const ConstantsBuilder = Java.type("org.ergoplatform.polyglot.ConstantsBuilder")
 
-print(MockData.infoFile)
+var res = new FileMockedRunner(MockData.infoFile, MockData.lastHeadersFile, MockData.boxFile).run(function (ctx) {
+    var mockTxB = ctx.newTxBuilder()
+    var out = mockTxB.outBoxBuilder()
+        .contract(ConstantsBuilder.empty(), "{ sigmaProp(CONTEXT.headers.size == 9) }")
+        .build()
+    var spendingTxB = ctx.newTxBuilder()
+    var tx = spendingTxB
+        .boxesToSpend(out.convertToInputWith(MockData.mockTxId, 0))
+        .outputs(
+            spendingTxB.outBoxBuilder()
+                .contract(ConstantsBuilder.empty(), "{ true }")
+                .build())
+        .build()
+    var proverB = ctx.newProver()
+    var prover = proverB.withSeed("abc").build()
+    var signed = prover.sign(tx)
+    return signed
+})
+
+print(res)
