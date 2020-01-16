9ec7a7bed55e44a83af5a0ba74d51a4d05c3fcdc
Alexander Slesarenko <avslesarenko@gmail.com>
2020-01-17 01:07:15 +0300

    towards ProveDHTuple spec

--- appkit/src/test/scala/org/ergoplatform/appkit/AnonimousAccessSpec.scala
+++ appkit/src/test/scala/org/ergoplatform/appkit/AnonimousAccessSpec.scala
@@ -2,21 +2,22 @@ package org.ergoplatform.appkit
 
 import org.scalatestplus.scalacheck.ScalaCheckDrivenPropertyChecks
 import org.scalatest.{PropSpec, Matchers}
+import sigmastate.basics.DLogProtocol.ProveDlog
 
 class AnonimousAccessSpec extends PropSpec with Matchers
     with ScalaCheckDrivenPropertyChecks
     with AppkitTesting
     with HttpClientTesting {
 
-  property("Get unspent boxes containing given address") {
-    val data = MockData(
-      Seq(
-        loadNodeResponse("response_Box1.json"),
-        loadNodeResponse("response_Box2.json"),
-        loadNodeResponse("response_Box3.json")),
-      Seq(
-        loadExplorerResponse("response_boxesByAddressUnspent.json")))
+  val data = MockData(
+    Seq(
+      loadNodeResponse("response_Box1.json"),
+      loadNodeResponse("response_Box2.json"),
+      loadNodeResponse("response_Box3.json")),
+    Seq(
+      loadExplorerResponse("response_boxesByAddressUnspent.json")))
 
+  property("Get unspent boxes containing given address") {
     val ergoClient = createMockedErgoClient(data)
 
     val boxes: java.util.List[InputBox] = ergoClient.execute { ctx: BlockchainContext =>
@@ -27,5 +28,23 @@ class AnonimousAccessSpec extends PropSpec with Matchers
     }
   }
 
+  // see original example in sigma https://github.com/ScorexFoundation/sigmastate-interpreter/blob/b3695bdb785c9b3a94545ffea506358ee3f8ed3d/sigmastate/src/test/scala/sigmastate/utxo/examples/DHTupleExampleSpecification.scala#L28
+  property("ProveDHTuple") {
+    val ergoClient = createMockedErgoClient(data)
+    val alice = Address.create("9f4QF8AD1nQ3nJahQVkMj8hFSVVzVom77b52JU7EW71Zexg6N8v")
+    val aliceDlog = alice.getPublicKey
+    val g_x = aliceDlog.value
+
+    val bob = Address.create("9hHDQb26AjnJUXxcqriqY1mnhpLuUeC81C4pggtK7tupr92Ea1K")
+    val bobDlog = bob.getPublicKey
+    val g_y = bobDlog.value
+
+    val res = ergoClient.execute { ctx: BlockchainContext =>
+      val bobProver = BoxOperations.createProver(ctx, "storage/E2.json", "abc")
+
+      ctx.getUnspentBoxesFor(Address.create(addr1))
+    }
+
+  }
 }
 
