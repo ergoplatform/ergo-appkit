e56903bae9a72ef9a6cb87511d27f2af75db56f6
Alexander Slesarenko <avslesarenko@gmail.com>
2020-01-18 01:11:11 +0300

    simple scenario with creating and proving DHT

--- appkit/src/test/scala/org/ergoplatform/appkit/AnonimousAccessSpec.scala
+++ appkit/src/test/scala/org/ergoplatform/appkit/AnonimousAccessSpec.scala
@@ -1,5 +1,10 @@
 package org.ergoplatform.appkit
 
+import java.util
+
+import org.ergoplatform.appkit.BoxOperations.{createProver, putToContractTx}
+import org.ergoplatform.appkit.ErgoContracts.sendToPK
+import org.ergoplatform.appkit.Parameters.MinFee
 import org.scalatestplus.scalacheck.ScalaCheckDrivenPropertyChecks
 import org.scalatest.{PropSpec, Matchers}
 import sigmastate.basics.DLogProtocol.ProveDlog
@@ -32,20 +37,54 @@ class AnonimousAccessSpec extends PropSpec with Matchers
   property("ProveDHTuple") {
     val ergoClient = createMockedErgoClient(data)
     val alice = Address.create("9f4QF8AD1nQ3nJahQVkMj8hFSVVzVom77b52JU7EW71Zexg6N8v")
+    val aliceStorage = "storage/E1.json"
     val aliceDlog = alice.getPublicKey
-    val g_x = aliceDlog.value
 
     val bob = Address.create("9hHDQb26AjnJUXxcqriqY1mnhpLuUeC81C4pggtK7tupr92Ea1K")
+    val bobStorage = "storage/E2.json"
     val bobDlog = bob.getPublicKey
     val g_y = bobDlog.value
 
     val res = ergoClient.execute { ctx: BlockchainContext =>
-      val aliceProver = BoxOperations.createProver(ctx, "storage/E1.json", "abc").build
-      val bobProver = BoxOperations.createProver(ctx, "storage/E2.json", "abc")
+
+      val aliceProver = createProver(ctx, aliceStorage, "abc").build
+      val g_x = aliceProver.getAddress.getPublicKeyGE
+
+      val bobProver = createProver(ctx, bobStorage, "abc")
               .withSecondDHTSecret(alice)
               .build
-//      val signed = BoxOperations.sendToContractTx(ctx, aliceProver, contract, Parameters.MinFee)
-//      println(signed.toJson(true))
+
+      val contract = ctx.compileContract(
+        ConstantsBuilder.create().item("g_x", g_x).build(),
+        """{
+         |  val g_y = OUTPUTS(0).R4[GroupElement].get
+         |  val g_xy = OUTPUTS(0).R5[GroupElement].get
+         |
+         |  proveDHTuple(g, g_x, g_y, g_xy) || // for bob
+         |  proveDHTuple(g, g_y, g_x, g_xy)    // for alice
+         |}""".stripMargin);
+      val aliceTx = putToContractTx(ctx, aliceProver, contract, MinFee)
+      val contractBox = aliceTx.getOutputsToSpend().get(0)
+      val amountOnContract = contractBox.getValue
+
+      val txB = ctx.newTxBuilder
+      val aliceBox = txB.outBoxBuilder
+        .value(amountOnContract)
+        .contract(sendToPK(ctx, aliceProver.getAddress)).build
+
+      val boxesToPayFee = BoxOperations.loadTop(ctx, bobProver.getAddress, MinFee)
+
+      val inputs = new util.ArrayList[InputBox]()
+      inputs.add(contractBox)
+      inputs.addAll(boxesToPayFee)
+
+      val tx = txB.boxesToSpend(inputs)
+        .outputs(aliceBox)
+        .fee(MinFee)
+        .sendChangeTo(bobProver.getP2PKAddress).build
+
+      val bobTx = bobProver.sign(tx)
+      println(bobTx.toJson(true))
     }
 
   }
