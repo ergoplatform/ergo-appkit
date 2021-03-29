183673ec671fbee2daf9395cb742ab1072bad97e
Alexander Slesarenko <avslesarenko@gmail.com>
2021-03-29 03:01:30 +0300

    added property("fromHex/toHex roundtrip")

--- common/src/test/scala/org/ergoplatform/appkit/ErgoValueSpec.scala
+++ common/src/test/scala/org/ergoplatform/appkit/ErgoValueSpec.scala
@@ -4,15 +4,22 @@ import org.scalatestplus.scalacheck.ScalaCheckDrivenPropertyChecks
 import org.scalatest.{PropSpec, Matchers}
 import scalan.RType
 import scorex.util.encode.Base16
-import sigmastate.{SCollection, SByte}
+import sigmastate._
 import sigmastate.Values.Constant
 import sigmastate.eval.SigmaDsl
 import sigmastate.serialization.ValueSerializer
+import sigmastate.serialization.generators.ObjectGenerators
 
 class ErgoValueSpec extends PropSpec with Matchers with ScalaCheckDrivenPropertyChecks
-  with AppkitTesting {
+  with AppkitTesting with ObjectGenerators {
 
   def Coll[T](items: T*)(implicit cT: RType[T]) = SigmaDsl.Colls.fromItems(items:_*)
+
+  def constToHex[T <: SType](c: Constant[T]): String = {
+    val bytes = ValueSerializer.serialize(c)
+    val hex = Base16.encode(bytes)
+    hex
+  }
 
   property("create from sigma Constant hex") {
     val hex = "05e012"
@@ -28,8 +35,7 @@ class ErgoValueSpec extends PropSpec with Matchers with ScalaCheckDrivenProperty
     )
 
     val c = Constant[SCollection[SCollection[SByte.type]]](coll, SCollection(SCollection(SByte)))
-    val bytes = ValueSerializer.serialize(c)
-    val hex = Base16.encode(bytes)
+    val hex = constToHex(c)
     hex shouldBe "1a0203010203020a14"
 
     val t = ErgoType.collType(ErgoType.byteType)
@@ -38,4 +44,24 @@ class ErgoValueSpec extends PropSpec with Matchers with ScalaCheckDrivenProperty
 
   }
 
+  property("fromHex/toHex roundtrip") {
+    def test[T <: SType](tpe: T) = {
+      implicit val wWrapped = wrappedTypeGen(tpe)
+      forAll { v: T#WrappedType =>
+        val c = Constant(v, tpe)
+        val hex = constToHex(c)
+        val ev = ErgoValue.fromHex(hex)
+        ev.toHex shouldBe hex
+        ev.getValue shouldBe v
+      }
+    }
+    test(SByte)
+    test(SShort)
+    test(SInt)
+    test(SLong)
+    test(SBigInt)
+    test(SGroupElement)
+    test(SSigmaProp)
+    test(SAvlTree)
+  }
 }
