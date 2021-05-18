d6b5cca646ef8272151e4066104ee3e2516f16a6
Alexander Slesarenko <avslesarenko@gmail.com>
2021-05-18 22:49:47 +0300

    zero-fee: more consistent validation in UnsignedTransactionBuilderImpl

--- lib-impl/src/main/java/org/ergoplatform/appkit/impl/UnsignedTransactionBuilderImpl.scala
+++ lib-impl/src/main/java/org/ergoplatform/appkit/impl/UnsignedTransactionBuilderImpl.scala
@@ -1,60 +1,63 @@
 package org.ergoplatform.appkit.impl
 
+import java.util
+
 import org.ergoplatform._
 import org.ergoplatform.appkit.{Iso, _}
 import org.ergoplatform.wallet.protocol.context.ErgoLikeStateContext
 import org.ergoplatform.wallet.transactions.TransactionBuilder
-import org.ergoplatform.wallet.boxes.{BoxSelector, DefaultBoxSelector}
-
+import org.ergoplatform.wallet.boxes.DefaultBoxSelector
 import special.collection.Coll
 import special.sigma.Header
 import java.util._
 import java.util.stream.Collectors
 
-import org.ergoplatform.appkit.Parameters.MinChangeValue
-import org.ergoplatform.appkit.Parameters.MinFee
+import org.ergoplatform.appkit.Parameters.{MinChangeValue, MinFee}
 import scorex.crypto.authds.ADDigest
-
-import scala.collection.JavaConversions
-import JavaHelpers._
+import org.ergoplatform.appkit.JavaHelpers._
 
 class UnsignedTransactionBuilderImpl(val _ctx: BlockchainContextImpl) extends UnsignedTransactionBuilder {
-  private[impl] val _inputs: List[UnsignedInput] = new ArrayList[UnsignedInput]
-  private[impl] val _dataInputs: List[DataInput] = new ArrayList[DataInput]
-  private[impl] var _outputCandidates: List[ErgoBoxCandidate] = new ArrayList[ErgoBoxCandidate]
-  private var _inputBoxes: List[InputBoxImpl] = null
-  private var _dataInputBoxes: List[InputBoxImpl] = new ArrayList[InputBoxImpl]
-  private val _tokensToBurn: List[ErgoToken] = new ArrayList[ErgoToken]
+  private[impl] var _inputs: List[UnsignedInput] = _
+  private var _inputBoxes: Option[List[InputBoxImpl]] = None
+
+  private[impl] var _dataInputs: List[DataInput] = new ArrayList[DataInput]()
+  private var _dataInputBoxes: Option[List[InputBoxImpl]] = None
+
+  private[impl] var _outputCandidates: Option[List[ErgoBoxCandidate]] = None
+  private var _tokensToBurn: Option[List[ErgoToken]] = None
   private var _feeAmount: Option[Long] = None
-  private var _changeAddress: ErgoAddress = null
-  private var _ph: PreHeaderImpl = null
+  private var _changeAddress: Option[ErgoAddress] = None
+  private var _ph: Option[PreHeaderImpl] = None
 
   override def preHeader(ph: PreHeader): UnsignedTransactionBuilder = {
-    require(_ph == null, "PreHeader is already specified")
-    _ph = ph.asInstanceOf[PreHeaderImpl]
+    require(_ph.isEmpty, "PreHeader is already specified")
+    _ph = Some(ph.asInstanceOf[PreHeaderImpl])
     this
   }
 
   override def boxesToSpend(inputBoxes: List[InputBox]): UnsignedTransactionBuilder = {
-    val items = inputBoxes
+    require(_inputBoxes.isEmpty, "boxesToSpend list is already specified")
+    _inputs = inputBoxes
       .map(box => JavaHelpers.createUnsignedInput(box.getId.getBytes))
-    _inputs.addAll(items)
-    _inputBoxes = inputBoxes.map(b => b.asInstanceOf[InputBoxImpl])
+    _inputBoxes = Some(inputBoxes.map(b => b.asInstanceOf[InputBoxImpl]))
     this
   }
 
   override def withDataInputs(inputBoxes: List[InputBox]): UnsignedTransactionBuilder = {
-    val items = inputBoxes
+    require(_dataInputBoxes.isEmpty, "dataInputs list is already specified")
+    _dataInputs = inputBoxes
       .map(box => JavaHelpers.createDataInput(box.getId.getBytes))
-    _dataInputs.addAll(items)
-    _dataInputBoxes = inputBoxes.map(_.asInstanceOf[InputBoxImpl])
+    _dataInputBoxes = Some(inputBoxes.map(_.asInstanceOf[InputBoxImpl]))
     this
   }
 
   override def outputs(outputs: OutBox*): UnsignedTransactionBuilder = {
     require(_outputCandidates.isEmpty, "Outputs already specified.")
-    _outputCandidates = new ArrayList[ErgoBoxCandidate]
-    appendOutputs(outputs:_*)
+    val candidates = outputs
+      .map(c => c.asInstanceOf[OutBoxImpl].getErgoBoxCandidate)
+      .toIndexedSeq.asInstanceOf[IndexedSeq[ErgoBoxCandidate]]
+      .convertTo[List[ErgoBoxCandidate]]
+    _outputCandidates = Some(candidates)
     this
   }
 
@@ -65,36 +68,46 @@ class UnsignedTransactionBuilderImpl(val _ctx: BlockchainContextImpl) extends Un
   }
 
   override def tokensToBurn(tokens: ErgoToken*): UnsignedTransactionBuilder = {
-    Collections.addAll(_tokensToBurn, tokens:_*)
+    require(_tokensToBurn.isEmpty, "Tokens to burn already specified.")
+    _tokensToBurn = Some({
+      val res = new util.ArrayList[ErgoToken]()
+      Collections.addAll(res, tokens:_*)
+      res
+    })
     this
-  }
-
-  private def appendOutputs(outputs: OutBox*): Unit = {
-    val boxes = outputs.map(c => c.asInstanceOf[OutBoxImpl].getErgoBoxCandidate)
-    Collections.addAll(_outputCandidates, boxes:_*)
   }
 
   override def sendChangeTo(changeAddress: ErgoAddress): UnsignedTransactionBuilder = {
-    require(_changeAddress == null, "Change address is already specified")
-    _changeAddress = changeAddress
+    require(_changeAddress.isEmpty, "Change address is already specified")
+    _changeAddress = Some(changeAddress)
     this
   }
 
+  def checkNonEmpty[T](list: Option[List[T]], msg: => String) = {
+    require(list.isDefined && !list.get.isEmpty, msg)
+  }
+
   override def build: UnsignedTransaction = {
-    val boxesToSpend = _inputBoxes
+    checkNonEmpty(_inputBoxes, "Input boxes are not specified")
+    checkNonEmpty(_outputCandidates, "Output boxes are not specified")
+    val boxesToSpend = _inputBoxes.get
       .map(b => ExtendedInputBox(b.getErgoBox, b.getExtension))
-    val dataInputBoxes = _dataInputBoxes.map(b => b.getErgoBox)
+    val dataInputBoxes = _dataInputBoxes
+       .getOrElse(new util.ArrayList[InputBoxImpl]())
+       .map(b => b.getErgoBox)
     val dataInputs = JavaHelpers.toIndexedSeq(_dataInputs)
     require(_feeAmount.isEmpty || _feeAmount.get >= MinFee,
       s"When fee amount is defined it should be >= $MinFee, got ${_feeAmount.get}")
-    require(_changeAddress != null, "Change address is not defined")
-    val outputCandidates = JavaHelpers.toIndexedSeq(_outputCandidates)
+    require(_changeAddress.isDefined, "Change address is not defined")
+    val outputCandidates = JavaHelpers.toIndexedSeq(_outputCandidates.get)
     val inputBoxes = JavaHelpers.toIndexedSeq(boxesToSpend.map(eb => eb.box))
-    val burnTokens = JavaHelpers.createTokensMap(Iso.isoJListErgoTokenToMapPair.to(_tokensToBurn))
+    val burnTokens = JavaHelpers.createTokensMap(
+      Iso.isoJListErgoTokenToMapPair.to(_tokensToBurn.getOrElse(new ArrayList[ErgoToken]))
+    )
     val tx = TransactionBuilder.buildUnsignedTx(
       inputs = inputBoxes, dataInputs = dataInputs, outputCandidates = outputCandidates,
       currentHeight = _ctx.getHeight, createFeeOutput = _feeAmount,
-      changeAddress = _changeAddress, minChangeValue = MinChangeValue,
+      changeAddress = _changeAddress.get, minChangeValue = MinChangeValue,
       minerRewardDelay = Parameters.MinerRewardDelay, burnTokens = burnTokens,
       boxSelector = DefaultBoxSelector).get
     val stateContext = createErgoLikeStateContext
@@ -108,7 +121,10 @@ class UnsignedTransactionBuilderImpl(val _ctx: BlockchainContextImpl) extends Un
 
     private val _headers = _allHeaders.slice(1, _allHeaders.length)
 
-    private val _preHeader = if (_ph == null) _ctx._preHeader._ph else _ph._ph
+    private val _preHeader = _ph match {
+      case Some(ph) => ph._ph
+      case _ => _ctx._preHeader._ph
+    }
 
     override def sigmaLastHeaders: Coll[Header] = _headers
 
@@ -120,14 +136,13 @@ class UnsignedTransactionBuilderImpl(val _ctx: BlockchainContextImpl) extends Un
 
   override def getCtx: BlockchainContext = _ctx
 
-  override def getPreHeader: PreHeader =
-    if (_ph == null) _ctx.getPreHeader else _ph
+  override def getPreHeader: PreHeader = _ph.getOrElse(_ctx.getPreHeader)
 
   override def outBoxBuilder = new OutBoxBuilderImpl(this)
 
   override def getNetworkType: NetworkType = _ctx.getNetworkType
 
   override def getInputBoxes: List[InputBox] =
-    _inputBoxes.stream.collect(Collectors.toList[InputBox])
+    _inputBoxes.get.stream.collect(Collectors.toList[InputBox])
 }
 
