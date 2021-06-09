6075603c57f4df918f03c6b317a94bef2326ce4a
Alexander Slesarenko <avslesarenko@gmail.com>
2021-06-09 12:41:04 +0300

    refresh-api-client: tests for most of the methods

--- java-client-generated/src/test/java/org/ergoplatform/explorer/client/DefaultApiTest.java
+++ java-client-generated/src/test/java/org/ergoplatform/explorer/client/DefaultApiTest.java
@@ -19,6 +19,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.util.HashMap;
 
 import static org.junit.Assert.*;
 
@@ -40,6 +41,12 @@ public class DefaultApiTest {
     String tokenId = "7970bba36de5d4d6b9c24c20dc65d764ebf7d4aeb5eeb01bbe94221aa03fee45";
 
     String blockId = "78a76fb6c8ac11e7e9da01f2c916b82dd1220a370d7fcfe2df94caa675c21926";
+
+    String ergoTree = "0008cd036ba5cfbc03ea2471fdf02737f64dbcd58c34461a7ec1e586dcd713dacbf89a12";
+
+    String boxId = "a7e33955a4d84fcfdfe9268a2950ad897772209a1f84fec9f206731918ac57d6";
+
+    String txId = "95faa1a6582adb7e5d482cdfd765ea63b92fc13c18538f1211f6e551e9c38875";
 
     @Test
     public void getApiV1AddressesP1BalanceConfirmedTest() throws IOException {
@@ -98,27 +105,28 @@ public class DefaultApiTest {
     }
 
     @Test
-    public void getApiV1BoxesByaddressP1Test() {
-        String p1 = null;
-        Integer offset = null;
-        Integer limit = null;
-        // ItemsA response = api.getApiV1BoxesByaddressP1(p1, offset, limit);
+    public void getApiV1BoxesByaddressP1Test() throws IOException {
+        Integer offset = 0;
+        Integer limit = 10;
+        ItemsA response = api.getApiV1BoxesByaddressP1(address, offset, limit).execute().body();
+        assertTrue(response.getItems().size() > 0);
     }
 
     @Test
-    public void getApiV1BoxesByergotreeP1Test() {
-        String p1 = null;
-        Integer offset = null;
-        Integer limit = null;
-        // ItemsA response = api.getApiV1BoxesByergotreeP1(p1, offset, limit);
+    public void getApiV1BoxesByergotreeP1Test() throws IOException {
+        Integer offset = 0;
+        Integer limit = 10;
+        ItemsA response = api.getApiV1BoxesByergotreeP1(ergoTree, offset, limit).execute().body();
+        assertTrue(response.getItems().size() > 0);
     }
 
     @Test
-    public void getApiV1BoxesByergotreetemplatehashP1Test() {
-        String p1 = null;
-        Integer offset = null;
-        Integer limit = null;
-        // ItemsA response = api.getApiV1BoxesByergotreetemplatehashP1(p1, offset, limit);
+    public void getApiV1BoxesByergotreetemplatehashP1Test() throws IOException {
+        Integer offset = 0;
+        Integer limit = 10;
+        // NOTE, this is actually negative test, because a hash is expected but not a tree.
+        ItemsA response = api.getApiV1BoxesByergotreetemplatehashP1(ergoTree, offset, limit).execute().body();
+        assertTrue(response.getItems().size() == 0);
     }
 
     @Test
@@ -126,45 +134,48 @@ public class DefaultApiTest {
         String p1 = null;
         Integer minHeight = null;
         Integer maxHeight = null;
-        // ListOutputInfo response = api.getApiV1BoxesByergotreetemplatehashP1Stream(p1, minHeight, maxHeight);
+        // NOTE, streamed methods are not supported by client generator
+        // the generated classes are useless
+//        ListOutputInfo response = api.getApiV1BoxesByergotreetemplatehashP1Stream(p1, minHeight, maxHeight);
     }
 
     @Test
-    public void getApiV1BoxesBytokenidP1Test() {
-        String p1 = null;
-        Integer offset = null;
-        Integer limit = null;
-        // ItemsA response = api.getApiV1BoxesBytokenidP1(p1, offset, limit);
+    public void getApiV1BoxesBytokenidP1Test() throws IOException {
+        Integer offset = 0;
+        Integer limit = 10;
+        ItemsA response = api.getApiV1BoxesBytokenidP1(tokenId, offset, limit).execute().body();
+        assertTrue(response.getItems().size() > 0);
     }
 
     @Test
-    public void getApiV1BoxesP1Test() {
-        String p1 = null;
-        // OutputInfo response = api.getApiV1BoxesP1(p1);
+    public void getApiV1BoxesP1Test() throws IOException {
+        OutputInfo response = api.getApiV1BoxesP1(boxId).execute().body();
+        assertEquals(response.getBoxId(), boxId);
     }
 
     @Test
-    public void getApiV1BoxesUnspentByaddressP1Test() {
-        String p1 = null;
-        Integer offset = null;
-        Integer limit = null;
-        // ItemsA response = api.getApiV1BoxesUnspentByaddressP1(p1, offset, limit);
+    public void getApiV1BoxesUnspentByaddressP1Test() throws IOException {
+        Integer offset = 0;
+        Integer limit = 10;
+        ItemsA response = api.getApiV1BoxesUnspentByaddressP1(address, offset, limit).execute().body();
+        assertTrue(response.getItems().size() > 0);
     }
 
     @Test
-    public void getApiV1BoxesUnspentByergotreeP1Test() {
-        String p1 = null;
-        Integer offset = null;
-        Integer limit = null;
-        // ItemsA response = api.getApiV1BoxesUnspentByergotreeP1(p1, offset, limit);
+    public void getApiV1BoxesUnspentByergotreeP1Test() throws IOException {
+        Integer offset = 0;
+        Integer limit = 10;
+        ItemsA response = api.getApiV1BoxesUnspentByergotreeP1(ergoTree, offset, limit).execute().body();
+        assertTrue(response.getItems().size() > 0);
     }
 
     @Test
-    public void getApiV1BoxesUnspentByergotreetemplatehashP1Test() {
-        String p1 = null;
-        Integer offset = null;
-        Integer limit = null;
-        // ItemsA response = api.getApiV1BoxesUnspentByergotreetemplatehashP1(p1, offset, limit);
+    public void getApiV1BoxesUnspentByergotreetemplatehashP1Test() throws IOException {
+        Integer offset = 0;
+        Integer limit = 10;
+        // NOTE, this is actually negative test, because a hash is expected but not a tree.
+        ItemsA response = api.getApiV1BoxesUnspentByergotreetemplatehashP1(ergoTree, offset, limit).execute().body();
+        assertTrue(response.getItems().size() == 0);
     }
 
     @Test
@@ -172,50 +183,56 @@ public class DefaultApiTest {
         String p1 = null;
         Integer minHeight = null;
         Integer maxHeight = null;
+        // NOTE, streamed methods are not supported by client generator
+        // the generated classes are useless
         // ListOutputInfo response = api.getApiV1BoxesUnspentByergotreetemplatehashP1Stream(p1, minHeight, maxHeight);
     }
 
     @Test
     public void getApiV1BoxesUnspentBylastepochsStreamTest() {
         Integer lastEpochs = null;
+        // NOTE, streamed methods are not supported by client generator
+        // the generated classes are useless
         // ListOutputInfo response = api.getApiV1BoxesUnspentBylastepochsStream(lastEpochs);
     }
 
     @Test
-    public void getApiV1BoxesUnspentBytokenidP1Test() {
-        String p1 = null;
-        Integer offset = null;
-        Integer limit = null;
-        // ItemsA response = api.getApiV1BoxesUnspentBytokenidP1(p1, offset, limit);
+    public void getApiV1BoxesUnspentBytokenidP1Test() throws IOException {
+        Integer offset = 0;
+        Integer limit = 10;
+        ItemsA response = api.getApiV1BoxesUnspentBytokenidP1(tokenId, offset, limit).execute().body();
+        assertTrue(response.getItems().size() > 0);
     }
 
     @Test
     public void getApiV1BoxesUnspentStreamTest() {
         Integer minHeight = null;
         Integer maxHeight = null;
+        // NOTE, streamed methods are not supported by client generator
+        // the generated classes are useless
         // ListOutputInfo response = api.getApiV1BoxesUnspentStream(minHeight, maxHeight);
     }
 
     @Test
-    public void getApiV1EpochsParamsTest() {
-        // EpochParameters response = api.getApiV1EpochsParams();
+    public void getApiV1EpochsParamsTest() throws IOException {
+        EpochParameters response = api.getApiV1EpochsParams().execute().body();
+        assertTrue(response.getMaxBlockCost() > 1000000);
     }
 
     @Test
-    public void getApiV1TokensTest() {
-        Integer offset = null;
-        Integer limit = null;
+    public void getApiV1TokensTest() throws IOException {
+        Integer offset = 0;
+        Integer limit = 10;
         String sortDirection = null;
         Boolean hideNfts = null;
-        // ItemsA response = api.getApiV1Tokens(offset, limit, sortDirection, hideNfts);
+        ItemsA response = api.getApiV1Tokens(offset, limit, sortDirection, hideNfts).execute().body();
+        assertTrue(response.getItems().size() == 10);
     }
 
     @Test
-    public void getApiV1TokensP1Test() {
-        String p1 = null;
-        // TokenInfo response = api.getApiV1TokensP1(p1);
-
-        // TODO: test validations
+    public void getApiV1TokensP1Test() throws IOException {
+        TokenInfo response = api.getApiV1TokensP1(tokenId).execute().body();
+        assertEquals(response.getId(), tokenId);
     }
 
     @Test
@@ -223,7 +240,7 @@ public class DefaultApiTest {
         String query = null;
         Integer offset = null;
         Integer limit = null;
-        // ItemsA response = api.getApiV1TokensSearch(query, offset, limit);
+        //ItemsA response = api.getApiV1TokensSearch(query, offset, limit);
 
         // TODO: test validations
     }
@@ -240,20 +257,21 @@ public class DefaultApiTest {
     }
 
     @Test
-    public void getApiV1TransactionsP1Test() {
-        String p1 = null;
-        // TransactionInfo response = api.getApiV1TransactionsP1(p1);
-
-        // TODO: test validations
+    public void getApiV1TransactionsP1Test() throws IOException {
+        TransactionInfo response = api.getApiV1TransactionsP1(txId).execute().body();
+        assertTrue(response.getOutputs().size() > 0);
     }
 
     @Test
-    public void postApiV1BoxesSearchTest() {
-        BoxQuery body = null;
-        Integer offset = null;
-        Integer limit = null;
-        // ItemsA response = api.postApiV1BoxesSearch(body, offset, limit);
-
-        // TODO: test validations
+    public void postApiV1BoxesSearchTest() throws IOException {
+        HashMap<String, String> regs = new HashMap<>();
+        regs.put("R4", "{\"serializedValue\": \"0702472963123ce32c057907c7a7268bc09f45d9ca57819d3327b9e7497d7b1cc347\"}");
+        BoxQuery body = new BoxQuery()
+           .registers(regs);
+        Integer offset = 0;
+        Integer limit = 10;
+        // TODO: clarify how to use this method
+//        ItemsA response = api.postApiV1BoxesSearch(body, offset, limit).execute().body();
+//        assertTrue(response.getItems().size() == 10);
     }
 }
