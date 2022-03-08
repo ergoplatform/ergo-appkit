e7f49e16fc33c90ab535e8ab3e2dca2751d177c7
Benjamin Schulte <MrStahlfelge@users.noreply.github.com>
2022-03-08 10:42:40 +0100

    BlockchainContextImpl check if reported network type matches given network type (#147)

--- common/src/main/java/org/ergoplatform/appkit/NetworkType.java
+++ common/src/main/java/org/ergoplatform/appkit/NetworkType.java
@@ -11,20 +11,35 @@ public enum NetworkType {
      * Mainnet network type.
      * @see ErgoAddressEncoder#MainnetNetworkPrefix()
      */
-    MAINNET(ErgoAddressEncoder.MainnetNetworkPrefix()),
+    MAINNET(ErgoAddressEncoder.MainnetNetworkPrefix(), "mainnet"),
 
     /**
      * Testnet network type.
      * @see ErgoAddressEncoder#TestnetNetworkPrefix()
      */
-    TESTNET(ErgoAddressEncoder.TestnetNetworkPrefix());
+    TESTNET(ErgoAddressEncoder.TestnetNetworkPrefix(), "testnet");
 
     /**
      * The network prefix code used in Ergo addresses
      */
     public final byte networkPrefix;
 
-    NetworkType(byte networkPrefix) {
+    /**
+     * verbose name for network type as reported by Node API
+     */
+    public final String verboseName;
+
+    NetworkType(byte networkPrefix, String verboseName) {
         this.networkPrefix = networkPrefix;
+        this.verboseName = verboseName;
+    }
+
+    public static NetworkType fromValue(String text) {
+        for (NetworkType t : NetworkType.values()) {
+            if (t.verboseName.equals(text)) {
+                return t;
+            }
+        }
+        return null;
     }
 }
