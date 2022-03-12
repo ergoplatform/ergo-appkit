1a1303d7ec5b68385bbbef33c989b2ebb7155040
Benjamin Schulte <MrStahlfelge@users.noreply.github.com>
2022-03-12 14:10:56 +0100

    Make verifySignature static on ErgoProver (#148)
    * Make verifySignature static and move from ErgoProver to Signature
    
    * Apply suggestions from code review
    
    Co-authored-by: Alexander Slesarenko <aslesarenko@users.noreply.github.com>

---
+++ common/src/main/java/org/ergoplatform/appkit/Signature.java
@@ -0,0 +1,38 @@
+package org.ergoplatform.appkit;
+
+import org.ergoplatform.P2PKAddress;
+import org.ergoplatform.ErgoLikeInterpreter;
+
+import sigmastate.Values.SigmaBoolean;
+import sigmastate.eval.CompiletimeIRContext;
+
+public class Signature {
+    private Signature() {
+        // prevent instantiation
+    }
+
+    /**
+     * Verifies a signature on given (arbitrary) message for a given public key.
+     *
+     * @param sigmaTree     public key (represented as a sigma proposition tree)
+     * @param message       message to verify
+     * @param signature signature for the message
+     * @return whether signature is valid or not
+     */
+    public static boolean verifySignature(SigmaBoolean sigmaTree, byte[] message, byte[] signature) {
+        return new ErgoLikeInterpreter((new CompiletimeIRContext())).verifySignature(sigmaTree, message, signature);
+    }
+
+    /**
+     * Verifies a signature on given (arbitrary) message
+     * using an address' public key.
+     *
+     * @param addr          address whose public key will be used to verify message
+     * @param message       message to verify
+     * @param signature signature for the message
+     * @return whether signature is valid or not
+     */
+    public static boolean verifySignature(P2PKAddress addr, byte[] message, byte[] signature) {
+        return new ErgoLikeInterpreter((new CompiletimeIRContext())).verifySignature(addr.pubkey(), message, signature);
+    }
+}
