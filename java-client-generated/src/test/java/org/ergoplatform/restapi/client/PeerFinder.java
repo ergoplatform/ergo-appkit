29418943a96a5e0965826142617c8cbcbae2ab99
Alexander Slesarenko <avslesarenko@gmail.com>
2023-10-31 15:42:39 +0100

    sigma-v5.0.12: update knownPeers

--- java-client-generated/src/test/java/org/ergoplatform/restapi/client/PeerFinder.java
+++ java-client-generated/src/test/java/org/ergoplatform/restapi/client/PeerFinder.java
@@ -6,9 +6,7 @@ public class PeerFinder extends ApiTestBase {
     public final String appVersion = "5.0.";
 
     String[] knownPeers = {
-        "198.58.96.195",
         "159.65.11.55",
-        "46.4.112.10",
         "213.239.193.208",
         "51.75.52.63",
         "159.65.11.55",
